5bcf78e0ae4fe364e1e37f1797fa311ba3d78298
李猫球 <pvoink@users.noreply.github.com>
2020-10-09 21:12:49 +0800

    。

--- js/mq-ss-div.js
+++ js/mq-ss-div.js
@@ -1,27 +1,3 @@
-var LocString = String(window.document.location.href); //?wz=
-function GetQueryString(name) {
-    var rs = new RegExp("(^|)" + name + "=([^&]*)(&|$)", "gi").exec(LocString), tmp;
-    if (tmp = rs) { return decodeURI(tmp[2]); }
-    return "";
-}
-var wzsc2 = GetQueryString("wz");
-$(function () {
-    $("#dataSet div").each(function () {
-        $(this).hide();
-        if ($(this).text().indexOf($.trim(wzsc2[0])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[1])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[2])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[3])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[4])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[5])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[6])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[7])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[8])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[9])) >= 0
-        ) { $(this).show(); }
-    })
-})
-
 $(function () { //搜索
     $("#searchBox").keyup(function () {
         var wzsc = $("#searchBox").val();
---
+++ js/mq-sswz-div.js
@@ -0,0 +1,23 @@
+var LocString = String(window.document.location.href);
+function GetQueryString(name) {
+    var rs = new RegExp("(^|)" + name + "=([^&]*)(&|$)", "gi").exec(LocString), tmp;
+    if (tmp = rs) { return decodeURI(tmp[2]); }
+    return "";
+}
+var wzsc2 = GetQueryString("wz");
+$(function () {
+    $("#dataSet div").each(function () {
+        $(this).hide();
+        if ($(this).text().indexOf($.trim(wzsc2[0])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[1])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[2])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[3])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[4])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[5])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[6])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[7])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[8])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[9])) >= 0
+        ) { $(this).show(); }
+    })
+})
