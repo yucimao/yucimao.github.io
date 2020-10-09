38435cfe7f48512e4949134b1c36b41da75ad282
李猫球 <pvoink@users.noreply.github.com>
2020-10-09 16:55:19 +0800

    。

--- js/mq-ss-div.js
+++ js/mq-ss-div.js
@@ -1,3 +1,27 @@
+var LocString = String(window.document.location.href); //?wz=
+function GetQueryString(name) {
+    var rs = new RegExp("(^|)" + name + "=([^&]*)(&|$)", "gi").exec(LocString), tmp;
+    if (tmp = rs) { return decodeURI(tmp[2]); }
+    return "";
+}
+var wzsc2 = GetQueryString("wz");
+$(function () {
+    $("#dataSet div").each(function () {
+        $(this).hide();
+        if ($(this).text().indexOf($.trim(wzsc2[0])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[1])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[2])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[3])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[4])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[5])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[6])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[7])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[8])) >= 0
+            && $(this).text().indexOf($.trim(wzsc2[9])) >= 0
+        ) { $(this).show(); }
+    })
+})
+
 $(function () { //搜索
     $("#searchBox").keyup(function () {
         var wzsc = $("#searchBox").val();
@@ -20,15 +44,15 @@ $(function () { //搜索
     })
 })
 
-function tion(){    //搜索按钮
+function tion() {    //搜索按钮
     $('#dataSet div').show();
     $("#dataSet ul").hide();
 
     $("#dataSet div").each(function () {
         $(this).hide();
-    
-    if ($(this).text().indexOf($.trim(sg)) >= 0) {
-                    $(this).show();
-                }
-            });
+
+        if ($(this).text().indexOf($.trim(sg)) >= 0) {
+            $(this).show();
+        }
+    });
 }
