df3b78a83d693e0174039473f5ec2b64bd5f6a56
李猫球 <pvoink@users.noreply.github.com>
2020-10-09 21:33:10 +0800

    。

--- js/mq-sswz-h4.js
+++ js/mq-sswz-h4.js
@@ -8,16 +8,7 @@ var wzsc2 = GetQueryString("wz");
 $(function () {
     $("#dataSet h4").each(function () {
         $(this).hide();
-        if ($(this).text().indexOf($.trim(wzsc2[0])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[1])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[2])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[3])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[4])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[5])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[6])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[7])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[8])) >= 0
-            && $(this).text().indexOf($.trim(wzsc2[9])) >= 0
+        if ($(this).text().indexOf($.trim(wzsc2)) >= 0
         ) { $(this).show(); }
     })
 })
