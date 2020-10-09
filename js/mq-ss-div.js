db69c269ca3a8fc343b755df76d61c7dac8d6ed1
李猫球 <pvoink@users.noreply.github.com>
2020-10-09 16:35:13 +0800

    。

--- js/mq-ss-div.js
+++ js/mq-ss-div.js
@@ -19,3 +19,16 @@ $(function () { //搜索
         })
     })
 })
+
+function tion(){    //搜索按钮
+    $('#dataSet div').show();
+    $("#dataSet ul").hide();
+
+    $("#dataSet div").each(function () {
+        $(this).hide();
+    
+    if ($(this).text().indexOf($.trim(sg)) >= 0) {
+                    $(this).show();
+                }
+            });
+}
--- js/mq-ss-h4.js
+++ js/mq-ss-h4.js
@@ -19,3 +19,16 @@ $(function () { //搜索
         })
     })
 })
+
+function tion(){    //搜索按钮
+    $('#dataSet h4').show();
+    $("#dataSet ul").hide();
+
+    $("#dataSet h4").each(function () {
+        $(this).hide();
+    
+    if ($(this).text().indexOf($.trim(sg)) >= 0) {
+                    $(this).show();
+                }
+            });
+}
