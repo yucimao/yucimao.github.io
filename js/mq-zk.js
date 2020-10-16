72ca77e3e8779d0b8559e3c05f000bb9e906a048
李猫球 <pvoink@users.noreply.github.com>
2020-10-16 15:52:39 +0800

    。

--- js/mq-zk.js
+++ js/mq-zk.js
@@ -86,7 +86,9 @@ mSwitch.init();
 
 function zk() {
     var yd = "／"+zka;
+    var dyy ;
+    dyy = "　";
     var dy = "<iframe src=\"/i/zk/" + zka + "\" frameborder=\"0\" width=\"100%\" height=\"100\" id=\"／／" +zka+ "\"></iframe><p id=\"加载\">加载中..</p>";
     document.getElementById(yd).innerHTML=dy;
-    document.getElementById(加载).innerHTML="　";
+    document.getElementById(加载).innerHTML=dyy;
 }
