60cc56738b45f52c6981fb4e1fd39d66b2b6ae34
李猫球 <123719932@qq.com>
2022-02-11 23:20:10 +0800

    。

--- wen/异梦笔记/0/1.js
+++ wen/异梦笔记/0/1.js
@@ -1,7 +1,8 @@
 
 let dy="<meta name='viewport' content='width=device-width, initial-scale=1'>";
-let 临时;
 let 博客="https://limaoqiu.com/";
+
+let wzjm="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",次数=0,制,客,乱,锁,临时;
 
 if(typeof(h1)!="undefined"){
 dy+="<h1>"+h1+"</h1>"
@@ -28,20 +29,37 @@ dy+="<br></div>";
 document.title = 临时;
 }
 
-if(typeof(sjtk)!="undefined"){dy+="<button onclick='sjtk_随机()'>随机</button> <a onclick='sjtk_全部显示()'>全部显示</a><div id='sjtk'></div>";}
+if(typeof(sjtk)!="undefined"){dy+="<button onclick='sjtk_随机()'>随机</button> <a onclick='sjtk_全部显示()'>全部显示</a>";
+if(typeof(sjtk_s)!="undefined"){dy+=" <input type='text' id='输入' placeholder='搜索' onkeyup='确定()'/>";}
+dy+="<div id='sjtk'></div>";}
 if(typeof(qm)!="undefined"){dy+="<button onclick='打(乱=qm);qm_随机()'>随机</button><input type='text' id='qm_输入' placeholder='抽取次数'/><br><br><div id='qm'></div>";}
 dy+="<div id='dy2'></div><textArea id='复制' style='position:absolute;top:-1000px'></textArea>";
 document.getElementById("dy").innerHTML = dy;
 
 
-
-let 制;function 复(){document.getElementById('复制').value = 制;let 复制 = document.querySelector('#复制');复制.select();document.execCommand("Copy");}
-let 客;function 博(){window.location.href=博客+客;}
-let 乱;function 打(){let _乱 = 乱.slice();for (let i = 0; i < _乱.length; i++) {let j = getRandomInt(0, i);let t = _乱[i];_乱[i] = _乱[j];_乱[j] = t;}function getRandomInt(min, max) {return Math.floor(Math.random() * (max - min + 1) + min);}乱=_乱;}
-
+function 复(){document.getElementById('复制').value = 制;let 复制 = document.querySelector('#复制');复制.select();document.execCommand("Copy");}
+function 博(){window.location.href=博客+客;}
+function 打(){let _乱 = 乱.slice();for (let i = 0; i < _乱.length; i++) {let j = getRandomInt(0, i);let t = _乱[i];_乱[i] = _乱[j];_乱[j] = t;}function getRandomInt(min, max) {return Math.floor(Math.random() * (max - min + 1) + min);}乱=_乱;}
 
 
-let 次数=0;
+function 确定() {
+    let 输入=document.getElementById("输入").value.toLowerCase().split(" ");
+    let 结果="";
+    for (let i=0;sjtk_s.length>i;i++){
+        let 过程="0";
+        let 关键词=sjtk_s[i].split(",");
+        for (let i2=0;输入.length>i2;i2++){
+            if (关键词[1].toLowerCase().indexOf(输入[i2]) != -1 && 输入[i2] != "") {}
+            else {过程="1";}
+        }
+        if (过程=="0" && sjtk.indexOf(关键词[0]) != -1){
+            结果+="<br><b><a onclick=\"复(制='"+关键词[0]+"')\">"+关键词[0]+"（删除码）</a></b>"+"<iframe src='1/"+关键词[0]+".html' height=100% width=100%></iframe><br>";
+        }
+    }
+    document.getElementById("sjtk").innerHTML = 结果;
+}
+
+
 function sjtk_随机(){
     if(次数==0){打(乱=sjtk);}
     if(乱.length==次数){
@@ -61,19 +79,22 @@ function sjtk_全部显示(){
 }
 function 删除码(){复(制="\""+sjtk[次数-1]+"\"");}
 
-if(typeof(qm)!="undefined"){乱=qm;临时="qm不随机";qm_随机();}
+if(typeof(qm)!="undefined"){for(let i=0;qm.length>i;i++){if(qm[i][0]=="js"){锁="／"+qm[i][1];解();qm[i]=[锁,"",""];}}乱=qm;临时="qm不随机";qm_随机();}
 function qm_随机(){
     let qm_输入 = document.getElementById("qm_输入").value;
     if(qm_输入>0){
         dy="<div class='qm'>";
-        for(let i=0;qm_输入>i&&乱.length>i;i++){
+        let i=0;
+        for(let i2=0;qm_输入>i2;i2++){
+            if(乱.length==i){i=0;dy+="<hr class='hr0'>";打();}
             if(乱[i][0]!="hr" && 乱[i][0]+乱[i][1]+乱[i][2]!=""){
-                if(乱[i][1]!=""||乱[i][2]!=""){//<a onclick="复(制='')"></a>
+                if(乱[i][1]!=""||乱[i][2]!=""){
                     dy+="<p onclick=\"复(制='"+乱[i][0]+"')\"><span class='提'>"+乱[i][0]+"<span class='示'><small>说明：</small>"+乱[i][1]+"<br><br><small>使用情况：</small>"+乱[i][2]+"</span></span></p>";
                 }else{
                     dy+="<p onclick=\"复(制='"+乱[i][0]+"')\">"+乱[i][0]+"</p>";
                 }
             }
+            i+=1;
         }
         dy+="</div>";
     }else{
@@ -89,8 +110,7 @@ function qm_随机(){
     document.getElementById("qm_输入").value=qm_输入;
 }
 
-let wzjm="ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-let 锁;function 解(){
+function 解(){
     let 锁id=锁.split("／")[0];
     锁=锁.split("／")[1];
     let wzjm_b="";
@@ -121,7 +141,7 @@ let 锁;function 解(){
     wzjm_c = new RegExp("／", "g");
     锁 = 锁.replace(wzjm_c, "");
     锁 = window.decodeURIComponent(atob(锁));
-    document.getElementById(锁id).innerHTML = 锁;
+    if(锁id!=""){document.getElementById(锁id).innerHTML = 锁;}
 }
 
 function 编辑(){
@@ -150,9 +170,12 @@ function 更新(){
             if (临时2[0].indexOf("|") != -1){
                 let 临时3=临时2[0].split("|");
                 if(临时3[0]=="解锁"){dy+="<span id='"+i+"' onclick=\"解(锁='"+i+"／"+临时3[1]+"')\" class='zhushi'></span>";}
+                else if(临时3[0]=="自动解锁"){锁="／"+临时3[1];解();dy+=锁;}
                 else if(临时3[0]=="提示"){dy+="<div class='提'>"+临时3[1]+"<span class='示'>"+临时3[2]+"</span></div>";}
                 else if(临时3[0]=="复制"){dy+="<a onclick=\"复(制='"+临时3[2]+"')\">"+临时3[1]+"</a>";}
-                else if(临时3[0]=="图片"){dy+="<a href='2/"+临时3[1]+".jpg' target='_blank'><img src='2/"+临时3[1]+".jpg' width="+临时3[2]+" "+临时3[3]+"></a>";}
+                else if(临时3[0]=="图片"){dy+="<a onclick=\"复(制='"+临时3[1]+"')\" href='2/"+临时3[1]+".jpg' target='_blank'><img src='2/"+临时3[1]+".jpg' width="+临时3[2]+" "+临时3[3]+"></a>";}
+                else if(临时3[0]=="本地图片"){dy+="<small>参考图片：</small><br><a onclick=\"复(制='"+临时3[1]+"')\" href='../../../../异梦笔记/0/2/"+临时3[1]+".jpg' target='_blank'><img src='../../../../异梦笔记/0/2/"+临时3[1]+".jpg' onerror=\"this.src='2/0.jpg';this.onerror=null;\" width="+临时3[2]+" "+临时3[3]+"></a>";}
+                else if(临时3[0]=="随机"){dy+="<div class='提'>"+临时3[1].split(",")[Math.floor(Math.random()*临时3[1].split(",").length)]+"<span class='示'>"+临时3[1]+"</span></div>";}
                 else if(临时3[0]=="随机题库"){
                     客="sjtk?t="+临时3[2];
                     if(临时3[3]){客+="&c="+临时3[3];}
@@ -178,6 +201,8 @@ function 更新(){
         dy=dy.replace(/ \| /g, "<span class=\"a\"> | </span>");
         dy=dy.replace(/ &gt; /g, "<span class=\"a\"> > </span>");
         dy=dy.replace(/<hr>/g, "<hr class='hr1'>");
+        dy=dy.replace(/“/g, "<b>“</b>");
+        dy=dy.replace(/”/g, "<b>”</b>");
         document.getElementById(乱).innerHTML = dy;
         }
     乱="dy";
@@ -188,4 +213,5 @@ function 更新(){
 window.onload = function () {
     if(window.location.href.indexOf("#") != -1){window.location.href=window.location.href;}
     更新();
+    if(window.location.href.indexOf("#") != -1){window.location.href=window.location.href;}
 }
