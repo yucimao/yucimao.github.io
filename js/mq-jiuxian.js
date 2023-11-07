53807a7603965d342735b1a5f6219b96baa7d941
LiMaoQiu <123719932@qq.com>
2023-11-07 20:48:12 +0800

    。

--- js/mq-jiuxian.js
+++ js/mq-jiuxian.js
@@ -4,7 +4,7 @@
 ［跳转］
 </div>
 <script>
-let 表={ //"名字_不同版本":["颜色","颜色／文字"], 酒、场景、人、价格 绿、白、黄 留空为灰
+let 表={ //"名字_不同版本":["颜色","颜色／文字"], 酒、水、人、白 绿、白、黄 留空为灰
 // "":["酒","／"],
 };
 let 表_代码={ //"按钮_跳转":"h4_按钮"
@@ -14,7 +14,7 @@ let 表_代码={ //"按钮_跳转":"h4_按钮"
 <script src="/js/mq-jiuxian.js"></script>
 */
 
-document.getElementById("酒仙").innerHTML="<style>#弹窗{position: absolute;left:0;position:fixed;bottom:0;margin:0 0 15px 20px;line-height:18px;padding:7px;width:300px;border-style:solid;border-radius:5px;border:2pxsolid #535353;background-color:#060f27d3;color:#b6b6b6;font-size:12px;z-index:1000;}#弹窗 .标题{color:#e4e4e4;font-weight:bold;font-size:14px;}#弹窗 .绿{color:#07F602;}#弹窗 .白{color:#e4e4e4;}#弹窗 .黄{color:#EBBC06;}#弹窗 .酒{color:#23A8F2;}#弹窗 .场景{color:#EBBC06;}#弹窗 .人{color:#07F602;}#弹窗 .价格{color:#6551B8;}#弹窗 .使用{color:#CCCCCC;}[onmousemove]{color:#006699;}[onmousemove].x{color:var(--body-color);border-bottom:1.8px solid #08f;}#right_anchor,#right_anchor2{display:none !important;}</style><div id=\"弹窗\" style=\"display:none;\"></div>";
+document.getElementById("酒仙").innerHTML="<style>#弹窗{position: absolute;left:0;position:fixed;bottom:0;margin:0 0 15px 20px;line-height:18px;padding:7px;width:300px;border-style:solid;border-radius:5px;border:2pxsolid #535353;background-color:#060f27d3;color:#b6b6b6;font-size:12px;z-index:1000;}#弹窗 .标题{color:#e4e4e4;font-weight:bold;font-size:14px;}#弹窗 .绿{color:#07F602;}#弹窗 .白{color:#e4e4e4;}#弹窗 .黄{color:#EBBC06;}#弹窗 .酒{color:#23A8F2;}#弹窗 .水{color:#3ffcff}#弹窗 .人{color:#07F602;}#弹窗 .使用{color:#CCCCCC;}[onmousemove]{color:#006699;}[onmousemove].x{color:var(--body-color);border-bottom:1.8px solid #08f;}</style><div id=\"弹窗\" style=\"display:none;\"></div>";
 
 let o="";
 let oo="";
@@ -47,7 +47,7 @@ let 跳转="";
             let 临时3="";
             if(表_代码[实际+"_跳转"]){临时3=" href=\"#"+表_代码[实际+"_跳转"]+"\""}
             if(表[实际]){
-                if(表[实际][0]=="酒"){文+="<a"+临时3+" onmousemove=\"进入(o='"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>"+临时[1];}
+                if(表[实际][0]=="酒"||表[实际][0]=="水"){if(表[实际][0]=="酒"){文+="<b>"}文+="<a"+临时3+" onmousemove=\"进入(o='"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>";if(表[实际][0]=="酒"){文+="</b>"}文+=临时[1]}
                 else{文+="<a"+临时3+" class=\"x\" onmousemove=\"进入(o='"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>"+临时[1];}
             }else{文+="<a"+临时3+" class=\"x\" onmousemove=\"进入(o='"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>"+临时[1];遗漏+="<br>"+实际;}
         }
@@ -65,8 +65,8 @@ function 进入(){
         for(let i=0;oo.length>i;i++){
             let 临时=oo[i].split("／");
             // if(临时[0]=="酒" && 临时[1]){dy+="<div class='标题 "+临时[1]+"'>"+o+"</div>";}
-            if(临时[0]=="价格"){dy+="<div class='标题 价格'>出售单价："+临时[1]+"</div>";}
-            else if(临时[0]=="代码"){
+            // if(临时[0]=="价格"){dy+="<div class='标题 价格'>出售单价："+临时[1]+"</div>";}else 
+            if(临时[0]=="代码"){
                 if(临时[2]){}else{临时[2]="使用";}
                 if(临时[1].length==2){dy+="<div class='标题 "+临时[2]+"'>"+表_代码[临时[1]]+"</div>";}
                 else{dy+="<div class='标题 "+临时[2]+"'>"+临时[1]+"</div>";}
