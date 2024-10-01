189e9a366a5e8bf40798bca903667178b2a469d7
LiMaoQiu <123719932@qq.com>
2024-10-02 01:56:28 +0800

    。

--- js/mq-jiuxian.js
+++ js/mq-jiuxian.js
@@ -1,26 +1,8 @@
-/*
-<div id="酒仙"></div>
-<div id="正文">
-［跳转］
-</div>
-<script>
-let 表={ //"名字_不同版本":["颜色","颜色／文字"], 酒、水、人、白 绿、白、黄 留空为灰
-// "":["酒","／"],
-};
-let 表_代码={ //"按钮_跳转":"h4_按钮"
-    "A":"内容",
-};
-</script>
-<script src="/js/mq-jiuxian.js"></script>
-*/
-
-document.getElementById("酒仙").innerHTML="<style>#弹窗{position: absolute;left:0;position:fixed;bottom:0;margin:0 0 15px 20px;line-height:18px;padding:7px;width:300px;border-style:solid;border-radius:5px;border:2pxsolid #535353;background-color:#060f27d3;color:#b6b6b6;font-size:12px;z-index:1000;}#弹窗 .标题{color:#e4e4e4;font-weight:bold;font-size:14px;}#弹窗 .绿{color:#07F602;}#弹窗 .白{color:#e4e4e4;}#弹窗 .黄{color:#EBBC06;}#弹窗 .酒{color:#23A8F2;}#弹窗 .水{color:#3ffcff}#弹窗 .人{color:#07F602;}#弹窗 .使用{color:#CCCCCC;}[onmousemove]{color:#006699;}[onmousemove].x{color:var(--body-color);border-bottom:1.6px solid #08f;}#right_anchor,#right_anchor2{display:none !important}[onmousemove^='进入(']{cursor:default}a[href]{cursor:pointer;}a[href]:after{font-size:0.6em;content:'⤴';color:#bbbbbb;}i{font-size:0.8em;color:#bbbbbb}</style><div id=\"弹窗\" style=\"display:none;\"></div>";
+document.getElementById("酒仙").innerHTML="<style>#弹窗{position:absolute;left:0;position:fixed;bottom:0;margin:0 0 15px 20px;line-height:18px;padding:7px;width:300px;border-style:solid;border-radius:5px;border:2pxsolid #535353;background-color:#060f27d3;color:#b6b6b6;font-size:12px;z-index:1000;}#弹窗 .标题{color:#e4e4e4;font-weight:bold;font-size:14px;}.提示.灰,#弹窗 .灰{color:#b6b6b6;}#弹窗 .绿{color:#07F602;}#弹窗 .蓝{color:#4646ff}#弹窗 .紫{color:#A827F7}#弹窗 .白{color:#e4e4e4;}#弹窗 .黄,#弹窗 .金{color:#EBBC06;}#弹窗 .红{color:#ff0000;}#弹窗 .酒{color:#23A8F2;}#弹窗 .水{color:#3fc5ff}#弹窗 .使用{color:#CCCCCC;}[onmousemove]{color:#006699;}[onmousemove].x{color:var(--body-color);border-bottom:1.2px solid #08f;}#right_anchor,#right_anchor2{display:none !important}[onmousemove^='进入(']{cursor:default}a[href]{cursor:pointer;}a[href]:after{font-size:0.6em;content:'⤴';color:#bbbbbb;}i{font-size:0.8em;color:#bbbbbb;font-weight:normal;letter-spacing:2px}</style><div id=\"弹窗\" style=\"display:none;\"></div>";
 
 let oo="";
-
 let 跳转="";
 
-// window.onload=function(){
     let 正=document.getElementById("正文").innerHTML.split("〈");
     let 文=正[0];
     let 遗漏=[];
@@ -38,50 +20,78 @@ let 跳转="";
         }else if(临时[0][0]=="目"&&临时[0][1]=="标"&&临时[0][2]=="|"){
             let 临时2=临时[0].split("|");
             文+="<a name='"+临时2[1]+"'></a>"+临时[1];
+        }else if(临时[0][0]=="?"&&临时[0][1]=="|"){
+            let 临时2=临时[0].split("|");
+            if(!临时2[2]){
+                临时2[2]="?";
+            }
+            文+="<sup class='提示 灰' data-msg='"+临时2[1]+"'>〔"+临时2[2]+"〕</sup>"+临时[1];
         }else{
             let 显示=临时[0];
             let 实际=显示;
-            if (显示.indexOf("|") != -1){显示=显示.split("|");实际=显示[1];显示=显示[0];}
+            if(显示.indexOf("|")!=-1){显示=显示.split("|");实际=显示[1];显示=显示[0];}
             显示=显示.split("_")[0];
+
             let 临时3="";
             if(表_代码[实际+"_跳转"]){临时3=" href=\"#"+表_代码[实际+"_跳转"]+"\""}
             if(表[实际]){
-                if(表[实际][0]=="酒"||表[实际][0]=="水"){if(表[实际][0]=="酒"){文+="<b>"}文+="<a"+临时3+" onmousemove=\"进入('"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>";if(表[实际][0]=="酒"){文+="</b>"}文+=临时[1]}
-                else{文+="<a"+临时3+" class=\"x\" onmousemove=\"进入('"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>"+临时[1];}
-            }else{文+="<a"+临时3+" class=\"x\" onmousemove=\"进入('"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>"+临时[1];遗漏[遗漏.length]=实际;}
+                if(表[实际][0][0]=="酒"){文+="[<a"+临时3+" onmousemove=\"进入('"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>]";文+=临时[1]}
+                else{文+="[<a"+临时3+" class=\"x\" onmousemove=\"进入('"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>]"+临时[1];}
+            }else{文+="[<a"+临时3+" class=\"x\" onmousemove=\"进入('"+实际+"')\" onmouseout=\"离开()\">"+显示+"</a>]"+临时[1];
+            遗漏[遗漏.length]=实际;表[实际]=[实际,"灰／“<b>"+实际+"</b>”条目存在错误，<br>请联系QQ<b>{{ site.mq_qq }}</b>反馈问题。"]}
         }
     }
-    if(遗漏.length!=0){
-        function removeDuplicates(array){
-            return [...new Set(array)];
-          }
-          遗漏=removeDuplicates(遗漏);
-        console.log("〈"+遗漏.join("〉〈")+"〉")
+    if(遗漏.length!=0){console.log("〈"+遗漏.join("〉〈")+"〉")}
+    文=文.replace(new RegExp('\\[',"g"),"<i> [<\/i>").replace(new RegExp('\\]',"g"),"<i>] <\/i>").replace(new RegExp('〔',"g"),"[").replace(new RegExp('〕',"g"),"]");
+    文=文.split("［跳转］");
+    文=文[0]+跳转+"<br><a href='#酒的列表'>酒的列表</a>"+文[1]+"<hr><a name='酒的列表'></a><h3>酒的列表</h3><ol>"
+
+    let keys = Object.keys(表);
+    keys.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+    for(let i=0;keys.length>i;i++){
+        if(表[keys[i]][0][0]=="酒"){
+            let 临时=表[keys[i]].join("／")+"／";
+
+            let 临时3=""
+            if(表_代码[keys[i]+"_跳转"]){表[keys[i]][表[keys[i]].length]="灰／(点击跳转)";临时3=" href=\"#"+表_代码[keys[i]+"_跳转"]+"\""}
+
+            if(临时.indexOf("／金／")!=-1){
+                临时=临时.split("／金／")[1].split("／")[0];
+                文+="<li><a"+临时3+" onmousemove=\"进入('"+keys[i]+"')\" onmouseout=\"离开()\">"+keys[i].split("_")[0]+"</a>："+临时+"</li>";
+            }else{
+                文+="<li><a"+临时3+" onmousemove=\"进入('"+keys[i]+"')\" onmouseout=\"离开()\">"+keys[i].split("_")[0]+"</a></li>";
+            }
+        }
+
+        
+
+        let dy="";
+        for(let i2=0;表[keys[i]].length>i2;i2++){
+            let 临时=表[keys[i]][i2].split("／");
+            // if(临时[0]=="酒" && 临时[1]){dy+="<div class='标题 "+临时[1]+"'>"+o+"</div>";}
+            if(临时[0]=="代码"){
+                if(表_代码[临时[1]]){临时=表_代码[临时[1]].split("／");}
+                else{console.log(临时[1])}
+            }
+
+            if(i2==0){
+                if(临时[2]&&临时[2]!=""){临时[0]=临时[2]}
+                if(临时[1]&&临时[1]!=""){dy+="<span class='标题 "+临时[0]+"'>"+临时[1]+"</span>"}
+                else{dy+="<span class='标题 "+临时[0]+"'>"+keys[i].split("_")[0]+"</span>"}
+
+            }else{if(!临时[2]&&临时[2]!="x"){dy+="<br>"}dy+="<span class='"+临时[0]+"'>"+临时[1]+"</span>";}
+        }
+        表[keys[i]]=dy;
     }
-    文=文.replace(new RegExp('\\[',"g"),"<i> [<\/i>").replace(new RegExp('\\]',"g"),"<i>] <\/i>");
-    文=文.split("［跳转］")
-    document.getElementById("正文").innerHTML = 文[0]+跳转+文[1];
-// }
+    文+="</ol>";
+    document.getElementById("正文").innerHTML=文;
+
 
 function 进入(o){
     if(表[o]){
         if(oo!=o){
-        oo=表[o];
-        let dy="";
-        for(let i=0;oo.length>i;i++){
-            let 临时=oo[i].split("／");
-            // if(临时[0]=="酒" && 临时[1]){dy+="<div class='标题 "+临时[1]+"'>"+o+"</div>";}
-            // if(临时[0]=="价格"){dy+="<div class='标题 价格'>出售单价："+临时[1]+"</div>";}else 
-            if(临时[0]=="代码"){
-                if(临时[2]){}else{临时[2]="使用";}
-                if(临时[1].length==2){dy+="<div class='标题 "+临时[2]+"'>"+表_代码[临时[1]]+"</div>";}
-                else{dy+="<div class='标题 "+临时[2]+"'>"+临时[1]+"</div>";}
-            }
-            else if(i==0 && 临时[1]){dy+="<div class='标题 "+临时[0]+"'>"+临时[1]+"</div>";}
-            else if(i==0){dy+="<div class='标题 "+临时[0]+"'>"+o.split("_")[0]+"</div>";}
-            else{dy+="<div class='"+临时[0]+"'>"+临时[1]+"</div>";}
-        }
-        document.getElementById("弹窗").innerHTML = dy;
+        document.getElementById("弹窗").innerHTML = 表[o];
     }
         oo=o;
     }
