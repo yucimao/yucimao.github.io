b63903bd18b0f0a6dec95239d184383e42a19a8a
LiMaoQiu <123719932@qq.com>
2025-04-09 11:40:45 +0800

    。

--- app/yxt/0.js
+++ app/yxt/0.js
@@ -20,7 +20,7 @@ if(window.location.href.indexOf("file:///")!=-1){页面dy+=" <small><a onclick='
 "<span id='余额'></span><hr><textArea id='复制' style='position:absolute;top:-1000px'></textArea>";
 document.getElementById("游戏厅").innerHTML=页面dy;
 
-let 制;function 复(){制=制.replace(/［换行］/g,'\n');notyf.confirm('已复制');document.getElementById('复制').value=制;let 复制=document.querySelector('#复制');复制.select();document.execCommand('Copy')}
+let 制;function 复(o){制=o||制;制=制.replace(/［换行］/g,'\n');notyf.confirm('已复制');document.getElementById('复制').value=制;let 复制=document.querySelector('#复制');复制.select();document.execCommand('Copy')}
 
 let 钱=0;if(localStorage.getItem("猫球游戏厅")){钱=localStorage.getItem("猫球游戏厅")*1;}
 let 钱_页面变化=钱;
